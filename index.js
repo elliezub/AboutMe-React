3deffc8cf8ccacb69625e164283e7ad5330dc3b8
Ellie <112726692+elliezub@users.noreply.github.com>
2023-07-15 14:17:27 -0400

    update

--- index.js
+++ index.js
@@ -1,14 +1,18 @@
-import React from "react"
-import ReactDOM from "react-dom"
-import Heading from "./Heading.js"
-import Hobbies from "./Hobbies.js"
-import Contact from "./Contact.js"
+import React from "react";
+import ReactDOM from "react-dom";
+import Heading from "./Heading.js";
+import Hobbies from "./Hobbies.js";
+import Contact from "./Contact.js";
 
 const App = () => {
   return (
     <div className="maindiv">
       <Heading name="Ellie Zubrowski" />
-      <Hobbies best="playing League of Legends" outdoor="swimming at the beach" indoor="learning to code"/>
+      <Hobbies
+        best="playing League of Legends"
+        outdoor="swimming at the beach"
+        indoor="learning to code"
+      />
       <Contact greeting="Contact me" callToAction="Email me" />
     </div>
   );
